9694dd2e5aa9355a07e2f89392d676e7092e279d
OCJune <cjbormk11@gmail.com>
2025-10-01 17:20:56 +0900

    [week3/mission] useParams를 활용한 영화 상세 페이지 개발

---
+++ Juno/Week3/Week3_Mission3/src/components/MovieCard.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import type { Movie } from "../types/movie";
+import { useNavigate } from "react-router-dom";
+
+interface MovieCardProps {
+  movie: Movie;
+  category: string;
+}
+
+function MovieCard({ movie: movie, category: category }: MovieCardProps) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const navigate = useNavigate();
+
+  return (
+    <div
+      onClick={() => navigate(`/movies/${category}/${movie.id}`)}
+      className="relative rounded-xl shadow-lg overflow-hidden cursor-pointer flex justify-between items-center w-44 h-62 transition-transform duration-300 hover:scale-105"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img
+        src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+        alt={`${movie.title}의 이미지`}
+      />
+      {isHovered && (
+        <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent backdrop-blur-md flex flex-col justify-center items-center text-white p-4">
+          <h2 className="text-lg font-bold leading-snug">{movie.title}</h2>
+          <p className="text-sm text-gray-300 leading-relaxed mt-2 line-clamp-5">
+            {movie.overview}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default MovieCard;
---
+++ Juno/Week3/Week3_Mission3/src/pages/MoviePage.tsx
@@ -0,0 +1,101 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import type { Movie, MovieResponse } from "../types/movie";
+import MovieCard from "../components/MovieCard";
+import LoadingSpinner from "../components/LoadingSpinner";
+
+function MoviePage() {
+  const param = useParams<{
+    category: string;
+  }>();
+  const category = param.category!; // category의 타입이 'string | undefined'에서 'string'으로 강제됨
+
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isPending, setIsPending] = useState(false); // 로딩 상태
+  const [isError, setIsError] = useState(false); // 에러 상태
+  const [page, setPage] = useState(1); // 페이지네이션
+  let totalPages;
+
+  useEffect(() => {
+    // TODO: Abort Controller 중복 요청 처리
+    const fetchMovies = async (url: string, options: object) => {
+      setIsPending(true); // 로딩 시작
+
+      try {
+        // const { data } = await axios(url, options);
+        const { data } = await axios.get<MovieResponse>(url, options); // GET 요청임을 명시
+        setMovies(data.results);
+        totalPages = data.total_pages;
+      } catch {
+        setIsError(true); // 에러 발생
+      } finally {
+        setIsPending(false); // 로딩 끝
+      }
+    };
+
+    const url = `https://api.themoviedb.org/3/movie/${category}?language=ko-KR&page=${page}`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
+      },
+    };
+
+    fetchMovies(url, options);
+  }, [page, category]); // useEffect에 page 전달: page값이 바뀔 때만 실행
+
+  if (isError) {
+    return (
+      <div>
+        <span className="text-red-500 text-2xl font-extrabold">
+          에러가 발생했습니다.
+        </span>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {/* TODO: component화 */}
+      <div className="flex justify-center items-center gap-6 mt-5">
+        <button
+          className="cursor-pointer bg-pink-300 text-white px-4 py-2 rounded-lg shadow-md hover:bg-pink-600 transition-all duration-200 disabled:bg-gray-300 disabled:cursor-not-allowed"
+          disabled={page === 1}
+          onClick={() => {
+            setPage((prev) => prev - 1);
+          }}
+        >
+          {"<"}
+        </button>
+        <span>{page} 페이지</span>
+        <button
+          className="cursor-pointer bg-pink-300 text-white px-4 py-2 rounded-lg shadow-md hover:bg-pink-600 transition-all duration-200 disabled:bg-gray-300 disabled:cursor-not-allowed"
+          disabled={page === totalPages}
+          onClick={() => {
+            setPage((prev) => prev + 1);
+          }}
+        >
+          {">"}
+        </button>
+      </div>
+
+      {isPending && (
+        <div className="flex items-center justify-center h-dvh">
+          <LoadingSpinner />
+        </div>
+      )}
+
+      {!isPending && (
+        <div className="p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} movie={movie} category={category} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+}
+
+export default MoviePage;
