e09e74484c58daa88232a6dc7f1d7d0f75861e21
kimkiyeonn <paynjoy2@naver.com>
2025-10-29 14:54:37 +0900

    [week5/mission1] mypage 수정

--- Sally/Week5/mission1/src/pages/Mypage.tsx
+++ Sally/Week5/mission1/src/pages/Mypage.tsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { getMyInfo } from "../apis/auth";
 import type { ResponseMyInfoDTO } from "../types/auth";
+import userImage from "../assets/user.png";
 
 const Mypage = () => {
-  const [data, setData] = useState<ResponseMyInfoDTO>([]);
+  const [data, setData] = useState<ResponseMyInfoDTO>();
   useEffect(() => {
     const getData = async () => {
       const response = await getMyInfo();
@@ -13,9 +14,17 @@ const Mypage = () => {
     getData();
   }, []);
   return (
-    <div className="h-dvh flex flex-col">
+    <div className="h-dvh flex flex-col items-center bg-gray-100">
       <Outlet />
-      <div>{data?.data?.name}</div>
+      <h1>My Page</h1>
+      <div className="flex flex-col items-center pt-5">
+        <img
+          src={userImage}
+          alt="User Profile"
+          className="w-32 h-32 rounded-full mb-4"
+        />
+        <div className="text-lg font-semibold">{data?.data?.name}</div>
+      </div>
     </div>
   );
 };
