215e8dac4403bcc801a6e787aab2d88ac28f5480
OCJune <cjbormk11@gmail.com>
2025-10-29 16:38:40 +0900

    [week5/mission] Social Login Google 직접 구현해보기

---
+++ Juno/Week5/Week5_Mission3/src/pages/MyPage.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+function MyPage() {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+  const handleLogout = async () => {
+    await logout();
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <h1>My Page</h1>
+      <button className="px-5 py-3 bg-blue-300" onClick={handleLogout}>
+        로그아웃
+      </button>
+    </div>
+  );
+}
+
+export default MyPage;
