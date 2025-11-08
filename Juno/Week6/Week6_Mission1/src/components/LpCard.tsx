85b18afab1baa32902be30897801e01c01008874
OCJune <cjbormk11@gmail.com>
2025-11-09 02:11:41 +0900

    [week6/mission] useQuery로 LP 목록/상세 화면 만들기

---
+++ Juno/Week6/Week6_Mission1/src/components/LpCard.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface LpCardProps {
+  id: number;
+  title: string;
+  createdAt: string;
+  thumnail: string;
+}
+
+const LpCard = ({ id, title, createdAt, thumnail }: LpCardProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const navigate = useNavigate();
+
+  return (
+    <div
+      onClick={() => navigate(`/lp/${id}`)}
+      className="relative rounded-xl shadow-lg overflow-hidden cursor-pointer flex justify-between items-center w-44 h-62 transition-transform duration-300 hover:scale-105"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img src={`${thumnail}`} alt={`${title}의 이미지`} />
+      {isHovered && (
+        <div className="absolute inset-0 bg-linear-to-t from-black/50 to-transparent backdrop-blur-md flex flex-col justify-center items-center text-white p-4">
+          <h2 className="text-lg font-bold leading-snug">{title}</h2>
+          <p className="text-sm text-gray-300 leading-relaxed mt-2 line-clamp-5">
+            {createdAt.slice(0, 10)}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LpCard;
