ef93f6347c7fa6faf8cd6f8e0767408004a59691
OCJune <cjbormk11@gmail.com>
2025-11-10 01:20:57 +0900

    [week6/mission] 무한스크롤 부분 useInfiniteQuery로 무한스크롤 구현해보기 + 스켈레톤 UI

---
+++ Juno/Week6/Week6_Mission2/src/components/LpCard/LpCard.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import type { Lp } from "../../types/lp";
+
+interface LpCardProps {
+  lp: Lp;
+}
+
+const LpCard = ({ lp }: LpCardProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const navigate = useNavigate();
+
+  return (
+    <div
+      onClick={() => navigate(`/lp/${lp.id}`)}
+      className="relative rounded-xl shadow-lg overflow-hidden cursor-pointer flex justify-between items-center w-44 h-62 transition-transform duration-300 hover:scale-105"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img src={`${lp.thumnail}`} alt={`${lp.title}의 이미지`} />
+      {isHovered && (
+        <div className="absolute inset-0 bg-linear-to-t from-black/50 to-transparent backdrop-blur-md flex flex-col justify-center items-center text-white p-4">
+          <h2 className="text-lg font-bold leading-snug">{lp.title}</h2>
+          <p className="text-sm text-gray-300 leading-relaxed mt-2 line-clamp-5">
+            {lp.createdAt.slice(0, 10)}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LpCard;
---
+++ Juno/Week6/Week6_Mission2/src/types/lp.ts
@@ -0,0 +1,51 @@
+import type { commonResponse, CursorBasedResponse } from "./common";
+
+export type Tag = {
+  id: number;
+  name: string;
+};
+
+export type Likes = {
+  id: number;
+  userId: number;
+  lpId: number;
+};
+
+export type Author = {
+  id: number;
+  name: string;
+  email: string;
+  bio: null | string;
+  avatar: null | string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type Lp = {
+  id: number;
+  title: string;
+  content: string;
+  thumnail: string;
+  published: boolean;
+  authorId: number;
+  updatedAt: string;
+  createdAt: string;
+  tags: Tag[];
+  likes: Likes[];
+};
+
+export type ResponseLpListDto = CursorBasedResponse<Lp[]>;
+
+export type ResponseLpDetailDto = commonResponse<{
+  id: number;
+  title: string;
+  content: string;
+  thumnail: string;
+  published: boolean;
+  authorId: number;
+  updatedAt: string;
+  createdAt: string;
+  tags: Tag[];
+  likes: Likes[];
+  author: Author;
+}>;
