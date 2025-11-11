7875cdff5396471bce49b0c78f7a2eddbc131075
OCJune <cjbormk11@gmail.com>
2025-11-11 22:28:20 +0900

    [week6/fix] 사이드바(Navbar.tsx)가 열려있는 채로 로딩되던 버그 수정

--- Juno/Week6/Week6_Mission2/src/hooks/useNavbar.tsx
+++ Juno/Week6/Week6_Mission2/src/hooks/useNavbar.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 const useNavbar = () => {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => {
     setIsOpen((prev) => !prev);
