4050aedd8aee896483c6c156ceca538e6c9dc5ec
OCJune <cjbormk11@gmail.com>
2025-11-11 20:30:04 +0900

    [week6/fix] LpCardSkeletonList.tsx의 문법 오류 수정

--- Juno/Week6/Week6_Mission2/src/components/LpCard/LpCardSkeletonList.tsx
+++ Juno/Week6/Week6_Mission2/src/components/LpCard/LpCardSkeletonList.tsx
@@ -7,9 +7,9 @@ interface LpCardSkeletonListProps {
 const LpCardSkeletonList = ({ count }: LpCardSkeletonListProps) => {
   return (
     <>
-      {new Array(count).fill(0).map((_, idx) => {
-        <LpCardSkeleton key={idx} />;
-      })}
+      {new Array(count).fill(0).map((_, idx) => (
+        <LpCardSkeleton key={idx} />
+      ))}
     </>
   );
 };
