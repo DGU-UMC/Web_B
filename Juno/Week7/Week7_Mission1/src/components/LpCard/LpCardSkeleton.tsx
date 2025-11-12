d82fe11e3667340589a1c2550a76f8f034d3df1b
OCJune <cjbormk11@gmail.com>
2025-11-12 18:14:18 +0900

    [week7/fix] tailwind css에 없는 h-62 대신 h-64로 수정

--- Juno/Week7/Week7_Mission1/src/components/LpCard/LpCardSkeleton.tsx
+++ Juno/Week7/Week7_Mission1/src/components/LpCard/LpCardSkeleton.tsx
@@ -1,6 +1,6 @@
 const LpCardSkeleton = () => {
   return (
-    <div className="animate-pulse relative rounded-xl shadow-lg overflow-hidden flex justify-between items-center w-44 h-62">
+    <div className="animate-pulse relative rounded-xl shadow-lg overflow-hidden flex justify-between items-center w-44 h-64">
       <div className="bg-gray-400 w-full h-full"></div>
     </div>
   );
