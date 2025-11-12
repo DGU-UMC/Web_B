6a38c08d839495be839539ccc78d4ad8ee2d4202
kiyeonkimm <paynjoy2@naver.com>
2025-11-12 12:48:48 +0900

    [week7/mission1] 모달 수정

--- Sally/Week6/mission1/src/components/CreateLpModal.tsx
+++ Sally/Week6/mission1/src/components/CreateLpModal.tsx
@@ -197,7 +197,6 @@ const CreateLpModal = ({ isOpen, onClose }: CreateLpModalProps) => {
         ref={modalRef}
         className="bg-white rounded-lg shadow-xl w-full max-w-2xl max-h-[90vh] overflow-y-auto m-4"
       >
-        {/* 헤더 */}
         <div className="flex items-center justify-between p-6 border-b border-gray-200">
           <h2 className="text-2xl font-bold">새 LP 작성</h2>
           <button
