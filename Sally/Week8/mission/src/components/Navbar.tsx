6b7df9a6b4cb29ebfe1a705a697f169bf0270117
kiyeonkimm <paynjoy2@naver.com>
2025-11-19 17:03:43 +0900

    [week8/mission3]  Sidebar 구현

--- Sally/Week8/mission/src/components/Navbar.tsx
+++ Sally/Week8/mission/src/components/Navbar.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { getMyInfo } from "../apis/auth";
+import { HamburgerButton } from "./HamburgerButton";
+import { useSidebar } from "../hooks/useSidebar";
+import { Sidebar } from "./Sidebar";
 
 const Navbar = () => {
   const { accessToken, logout } = useAuth();
@@ -27,59 +30,64 @@ const Navbar = () => {
     setUserName(null);
     navigate("/");
   };
+  const { isOpen, toggle, close } = useSidebar();
 
   return (
-    <nav className="bg-gray-800 fixed w-full z-10">
-      <div className="flex items-center justify-between p-4">
-        <div className="flex items-center space-x-3">
-          <Link to="/" className="text-xl font-bold text-white">
-            돌려돌려 LP판
-          </Link>
-        </div>
-        <div className="space-x-6">
-          {!accessToken && (
+    <>
+      <nav className="bg-gray-800 fixed w-full z-10">
+        <div className="flex items-center justify-between p-4">
+          <div className="flex items-center space-x-3">
+            <HamburgerButton isOpen={isOpen} onClick={toggle} />
+            <Link to="/" className="text-xl font-bold text-white">
+              돌려돌려 LP판
+            </Link>
+          </div>
+          <div className="space-x-6">
+            {!accessToken && (
+              <>
+                <Link
+                  to={"/login"}
+                  className="text-gray-300 darjk:text-gray-300 hover:text-pink-500 "
+                >
+                  로그인
+                </Link>
+                <Link
+                  to={"/signup"}
+                  className="text-gray-300 darjk:text-gray-300 hover:text-pink-500 "
+                >
+                  회원가입
+                </Link>
+              </>
+            )}
+          </div>
+
+          {accessToken && (
             <>
-              <Link
-                to={"/login"}
-                className="text-gray-300 darjk:text-gray-300 hover:text-pink-500 "
-              >
-                로그인
-              </Link>
-              <Link
-                to={"/signup"}
-                className="text-gray-300 darjk:text-gray-300 hover:text-pink-500 "
-              >
-                회원가입
-              </Link>
+              <div className="flex items-center space-x-4">
+                {userName && (
+                  <span className="sm:inline text-gray-300 text-sm md:text-base">
+                    {userName}님 반갑습니다.
+                  </span>
+                )}
+                <Link
+                  to={"/mypage"}
+                  className="text-gray-300 darjk:text-gray-300 hover:text-pink-500 " // 모바일에서 숨김
+                >
+                  마이페이지
+                </Link>
+                <button
+                  onClick={handleLogout}
+                  className="text-gray-300 darjk:text-gray-300 hover:text-pink-500 " // 모바일에서 숨김
+                >
+                  로그아웃
+                </button>
+              </div>
             </>
           )}
         </div>
-
-        {accessToken && (
-          <>
-            <div className="flex items-center space-x-4">
-              {userName && (
-                <span className="sm:inline text-gray-300 text-sm md:text-base">
-                  {userName}님 반갑습니다.
-                </span>
-              )}
-              <Link
-                to={"/mypage"}
-                className="text-gray-300 darjk:text-gray-300 hover:text-pink-500 " // 모바일에서 숨김
-              >
-                마이페이지
-              </Link>
-              <button
-                onClick={handleLogout}
-                className="text-gray-300 darjk:text-gray-300 hover:text-pink-500 " // 모바일에서 숨김
-              >
-                로그아웃
-              </button>
-            </div>
-          </>
-        )}
-      </div>
-    </nav>
+      </nav>
+      <Sidebar isOpen={isOpen} onClose={close} />
+    </>
   );
 };
 
