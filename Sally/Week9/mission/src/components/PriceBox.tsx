2990a1f2654486554721e72a9cf4a5451d68541a
kiyeonkimm <paynjoy2@naver.com>
2025-12-01 15:04:37 +0900

    [week9/mission2] Modal Slice

--- Sally/Week9/mission/src/components/PriceBox.tsx
+++ Sally/Week9/mission/src/components/PriceBox.tsx
@@ -1,12 +1,13 @@
 import { useDispatch, useSelector } from "../hooks/useCustomRedux";
-import { clearCart } from "../slices/cartSlice";
+import { openModal } from "../slices/modalSlice";
 
 const PriceBox = () => {
   const { total } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   const handleInitializeCart = () => {
-    dispatch(clearCart());
+    // open confirmation modal instead of clearing directly
+    dispatch(openModal());
   };
 
   return (
