6a7838db2331200945f92e5cf36c4295003ec9b3
OCJune <cjbormk11@gmail.com>
2025-12-20 20:15:51 +0900

    [week10/practice] 실습2 - useMemo는 memo와 다른 것일까? 에라토테네스의 체로도 최적화 도전해보기

---
+++ Juno/Week10/Week10_Practice2/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import UseMemoPage from "./pages/useMemoPage";
+
+function App() {
+  return (
+    <div className="h-dvh flex flex-col justify-center items-center">
+      <UseMemoPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ Juno/Week10/Week10_Practice2/src/components/TextInput.tsx
@@ -0,0 +1,17 @@
+interface ITextInput {
+  onChange: (text: string) => void;
+}
+
+const TextInput = ({ onChange }: ITextInput) => {
+  console.log("TextInput 렌더링됨");
+
+  return (
+    <input
+      type="text"
+      className="border p-2 rounded-lg"
+      onChange={(e) => onChange(e.target.value)}
+    ></input>
+  );
+};
+
+export default TextInput;
